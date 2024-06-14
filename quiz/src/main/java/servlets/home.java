d4ea15bef69d5ab15369bc967f0022a3234e2855
azhgh22 <azhgh22@freeuni.edu.ge>
2024-06-14 15:12:06 +0400

    seting up login doPost

--- quiz/src/main/java/servlets/home.java
+++ quiz/src/main/java/servlets/home.java
@@ -1,26 +1,26 @@
 package servlets;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-public class login extends HttpServlet {
-
+public class home extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("login_signup.jsp").forward(request,response);
+        // if logged in show user home page
+        // else redirect to /login
+
+        if(false){
+
+        }else{
+            response.sendRedirect("/login");
+        }
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-
 
     }
 }
--- quiz/src/main/java/servlets/signup.java
+++ quiz/src/main/java/servlets/signup.java
@@ -1,15 +1,17 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class signup extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").forward(request,response);
+        request.getRequestDispatcher("login_signup.jsp").forward(request,response);
     }
 
     @Override
