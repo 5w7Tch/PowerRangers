b0772b9aad390ce60cf7a748910e445df0f8227c
azhgh22 <azhgh22@freeuni.edu.ge>
2024-06-15 18:51:24 +0400

    finished signup servlet

--- quiz/src/main/java/models/DAO/dbCredentials.java
+++ quiz/src/main/java/models/DAO/dbCredentials.java
@@ -3,5 +3,5 @@ package models.DAO;
 public interface dbCredentials {
     String url = "jdbc:mysql://localhost:3306/quizdb";
     String userName = "root";
-    String password = "rootroot";
+    String password = "root@root";
 }
