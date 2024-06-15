91f41fbfcd39c4e6f0320c579b166c15a2d01c81
azhgh22 <azhgh22@freeuni.edu.ge>
2024-06-15 20:37:36 +0400

    started creating of home page for user

--- quiz/src/main/java/servlets/login.java
+++ quiz/src/main/java/servlets/login.java
@@ -1,11 +1,9 @@
 package servlets;
 
 import models.DAO.Dao;
-import models.DAO.mySqlDb;
 import models.USER.User;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
