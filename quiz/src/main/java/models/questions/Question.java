91999ed79bdf2f376d08bac8a343a39e2082ebce
5w7tch <nurch22@freeuni.edu.ge>
2024-06-17 18:03:16 +0400

    interfase for questions

--- quiz/src/main/java/models/questions/Question.java
+++ quiz/src/main/java/models/questions/Question.java
@@ -1,4 +1,16 @@
 package models.questions;
 
 public interface Question {
+    enum questionTypes{
+        questionResponse,
+        fillInTheBlank,
+        multipleChoice,
+        pictureResponseQuestion,
+        multipleAnswerQuestions,
+        multipleChoiceWithMultipleAnswers,
+        matching
+    }
+    String getQuestion();
+    String getAnswer();
+    
 }
