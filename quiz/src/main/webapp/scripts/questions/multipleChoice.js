224c92608901e0625728f50741b1801d4bb0dd86
5w7tch <nurch22@freeuni.edu.ge>
2024-06-26 18:23:30 +0400

    ignore

--- quiz/src/main/webapp/scripts/questions/multipleChoice.js
+++ quiz/src/main/webapp/scripts/questions/multipleChoice.js
@@ -21,12 +21,24 @@ export class multipleChoice{
                     var answersContainer = document.getElementById('answersContainer');
                     var textareaId = '' + answersContainer.children.length;
                     var textarea = document.createElement('textarea');
+                    var deleteBtn = document.createElement('button');
+                    deleteBtn.id = textareaId;
+                    deleteBtn.name = "delete";
+                    deleteBtn.addEventListener('click', function() {
+                        answersContainer.removeChild(textarea);
+                        answersContainer.removeChild(deleteBtn);
+                    });
+                    deleteBtn.style.backgroundColor = "#ff0000";
                     textarea.className = 'form-control';
                     textarea.style.marginBottom = '5px';
                     textarea.id = textareaId;
                     textarea.rows = 3;
+                    answersContainer.appendChild(deleteBtn);
                     answersContainer.appendChild(textarea);
+                    
               });
+              
+        
             </script>
         `;
     }
@@ -44,13 +56,31 @@ export class multipleChoice{
     setPreview(){
         $('#question').text(this.question);
         $('#answer').text(this.answer);
-        var answersHtml = '';
         let idx = 0;
+        var answersContainer = document.getElementById('answersContainer');
+        answersContainer.innerHTML = '';
         this.answers.forEach(function(answer) {
-            answersHtml += '<textarea class="form-control" id="'+ idx+'" rows="3" >'+ answer +'</textarea>';
-            idx++;
+            if(answer.length !== 0){
+                // answersHtml += '<textarea class="form-control" id="'+ idx+'" rows="3" style="margin-bottom: 5px;">'+ answer +'</textarea>';
+                var textarea = document.createElement('textarea');
+                var deleteBtn = document.createElement('button');
+                deleteBtn.id = idx.toString();
+                deleteBtn.name = "delete";
+                deleteBtn.addEventListener('click', function() {
+                    answersContainer.removeChild(textarea);
+                    answersContainer.removeChild(deleteBtn);
+                });
+                deleteBtn.style.backgroundColor = "#ff0000";
+                textarea.className = 'form-control';
+                textarea.style.marginBottom = '5px';
+                textarea.id = '' + idx;
+                textarea.rows = 3;
+                textarea.value = answer;
+                answersContainer.appendChild(deleteBtn);
+                answersContainer.appendChild(textarea);
+                idx++;
+            }
         });
-        $('#answersContainer').html(answersHtml);
     }
 
     generateJson() {
