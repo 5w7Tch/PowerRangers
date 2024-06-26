3c8847a166d194d9424334db2dc30ad6f70f3478
5w7tch <nurch22@freeuni.edu.ge>
2024-06-26 15:38:30 +0400

    ignore

--- quiz/src/main/java/servlets/quizHome.java
+++ quiz/src/main/java/servlets/quizHome.java
@@ -1,16 +1,29 @@
 package servlets;
 
+import models.DAO.Dao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class quizHome extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("generateQuizHomePage.jsp").forward(request,response);
+        String quiz = request.getParameter("quizid");
+        Dao myDb = (Dao)request.getServletContext().getAttribute(Dao.DBID);
+        try {
+            if(quiz != null && myDb.getQuiz(quiz) != null){
+                request.getRequestDispatcher("generateQuizHomePage.jsp").forward(request,response);
+            }else{
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
